f536e0ff5713571585f638a5b87783a5eda8a3ad
Mohamed Suja <mohamedsuja7n@gmail.com>
2024-05-04 12:11:22 +0530

    update

--- src/main/java/com/sliderz/BeanConfig.java
+++ src/main/java/com/sliderz/BeanConfig.java
@@ -1,2 +1,9 @@
-package com.sliderz;public class BeanConfig {
+package com.sliderz;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.sliderz")
+public class BeanConfig {
 }
--- src/main/java/com/sliderz/ShoppingCart.java
+++ src/main/java/com/sliderz/ShoppingCart.java
@@ -1,2 +1,12 @@
-package com.sliderz;public class ShoppingCart {
+package com.sliderz;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShoppingCart {
+
+    public void checkout() {
+
+        System.out.println("Checkout");
+    }
 }
